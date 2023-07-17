25d22cd672ed9661b1e3969bde5c5afa3d64d99e
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2023-07-17 14:43:48 +0300

    Constructor injection

--- 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/ibrahimbayburtlu/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/ibrahimbayburtlu/springcoredemo/rest/DemoController.java
@@ -1,5 +1,6 @@
-package com.ibrahimbayburtlu.springcoredemo;
+package com.ibrahimbayburtlu.springcoredemo.rest;
 
+import com.ibrahimbayburtlu.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ public class DemoController {
 
     private Coach myCoach;
 
-    // define a constructor for dependency injcetion
+    // define a constructor for dependency injection
 
     @Autowired
     public DemoController(Coach theCoach){
