a36026e0787283e9a591c9b49e8b3e8c549c322e
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2023-07-17 21:56:43 +0300

    Qualifiers

--- 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/ibrahimbayburtlu/springcoredemo/common/TennisCoach.java
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/ibrahimbayburtlu/springcoredemo/common/TennisCoach.java
@@ -3,10 +3,9 @@ package com.ibrahimbayburtlu.springcoredemo.common;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CricketCoach implements Coach {
-
+public class TennisCoach implements Coach{
     @Override
     public String getDailyWorkout() {
-        return "Practice fast bowling for 15 minutes!!!";
+        return "Practice your backhand volley";
     }
 }
--- 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/ibrahimbayburtlu/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/ibrahimbayburtlu/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.ibrahimbayburtlu.springcoredemo.rest;
 
 import com.ibrahimbayburtlu.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public void setMyCoach(Coach theCoach){
+    public DemoController(@Qualifier("baseballCoach") Coach theCoach){
         myCoach = theCoach;
     }
 
