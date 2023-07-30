d5c8a53914a81ab2211f4a84473db05eca23834b
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2023-07-30 15:07:59 +0300

    Qualifier

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/ibrahimbayburtlu/springcoredemo/common/Coach.java
@@ -0,0 +1,6 @@
+package com.ibrahimbayburtlu.springcoredemo.common;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/ibrahimbayburtlu/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,8 @@
+package com.ibrahimbayburtlu.springcoredemo.common;
+
+public class TrackCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+}
