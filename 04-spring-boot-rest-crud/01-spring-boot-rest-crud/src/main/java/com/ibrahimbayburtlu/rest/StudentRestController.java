8713fe13ee4506c7b4624b8741ff21db7ff36760
ibrahim Bayburtlu <69054547+ibrahimbayburtlu@users.noreply.github.com>
2023-08-04 19:37:54 +0300

    Global Exception Handler

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/ibrahimbayburtlu/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/ibrahimbayburtlu/rest/StudentRestController.java
@@ -2,9 +2,10 @@ package com.ibrahimbayburtlu.rest;
 
 import com.ibrahimbayburtlu.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,35 +43,5 @@ public class StudentRestController {
 
         // just index into the list ... keep it simple for now
         return theStudents.get(studentId);
-    }
-
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
-    }
-
-    // add another exception handler ... to catch any exception (catch all)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
-
     }
 }
